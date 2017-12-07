90a30b88092293d2b9788757acc9dd207048139b
janto <ja.merino@usp.ceu.es>
2017-12-07 14:44:27 +0100

    he añadido el filtro VerificarAutenticidad (entre el html y la servlet) el cual verifica que si tu user y password son ram puedes acceder al contenido de la servlet pero sino te redirecciona al html, si no logras superar porque el usuario y la password no son ram, aunque en el browserr pongas la direccion de la servlet no te dejara aacceder a ella

---
+++ src/java/Filter/VerificarAutenticidad.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Filter;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author janto
+ */
+public class VerificarAutenticidad implements Filter {
+    
+
+
+    /**
+     *
+     * @param request The servlet request we are processing
+     * @param response The servlet response we are creating
+     * @param chain The filter chain we are processing
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet error occurs
+     */
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        String user = req.getParameter("user");
+        String password= req.getParameter("password");
+        if (!(user.equalsIgnoreCase("ram") && password.equals("ram")))
+        {
+               System.out.println("Unauthorized request");
+            System.out.println("######################################################\n\n");
+            res.sendRedirect("index.html");
+        }
+        else
+        {
+            // pass the request along the filter chain
+            chain.doFilter(request, response);
+        }
+
+    }
+
+    /**
+     * Destroy method for this filter
+     */
+    public void destroy() {        
+    }
+
+    /**
+     * Init method for this filter
+     */
+    public void init(FilterConfig filterConfig) {        
+      
+    }
+
+    
+    
+}
